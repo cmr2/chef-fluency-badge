72a5dabdaa46434b2d32ab9f30871a0bf605de24
C R <killervirgo9879@gmail.com>
2017-06-08 23:48:43 +0000

    added the postgresql cookbook

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,7 +5,7 @@ log_level                :info
 log_location             STDOUT
 node_name                "clint"
 client_key               "#{current_dir}/clint.pem"
-validation_client_name   "clintrichardson-validator.pem"
-validation_key           "#{current_dir}/clintrichardson-validator.pem"
+#validation_client_name   "clintrichardson-validator.pem"
+#validation_key           "#{current_dir}/clintrichardson-validator.pem"
 chef_server_url          "https://chef-server-instance.c.crucial-weaver-168401.internal/organizations/clintrichardson"
 cookbook_path            ["#{current_dir}/../cookbooks"]
